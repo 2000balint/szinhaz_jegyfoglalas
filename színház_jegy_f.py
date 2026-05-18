a285cbe3c8b580cbee0986bd240e216359d62bd9
GAkos2007 <gaal.akos10@gmail.com>
2026-05-18 16:33:58 +0200

    Add files via upload

--- színház_jegy_f.py
+++ színház_jegy_f.py
@@ -1,15 +1,19 @@
 adatbazis=[]
 with open("adatbazis.txt","rt",encoding="utf-8")as f:
-    fejlec = f.readline()
+    fejlec=f.readline()
     for sor in f:
-        sor_adatok = sor.strip().split(";")
-        if len(sor_adatok) == 3:
+        sor_adatok=sor.strip().split(";")
+        if len(sor_adatok)==3:
             adatbazis.append(sor_adatok)
-print(adatbazis)
 
-szindarab=input("Mit szeretnél nézni? ")
+szindarab=input("Mit szeretnél nézni?\n"
+"-> Pál utcai fiúk\n"
+"-> Dzsungel könyve\n"
+"-> A diktátor\n"
+"-> Frankenstein\n"
+)
 
-print(f"\nA(z) {szindarab} időpontjai:")
+print(f"\nA {szindarab} időpontjai:")
 print("-" * 40)
  
 for eloadas in adatbazis:
@@ -17,5 +21,18 @@ for eloadas in adatbazis:
     idopont=eloadas[1]
     cim=eloadas[2]
     
-    if szindarab.lower() in cim.lower():
+    if szindarab in cim:
         print(f"-> {nap}-{idopont}-kor")
+print()
+mikor=input("Mikor szeretnéd nézni? ")
+print("-" * 40)
+print("Szuper! Következő lépés a helyfoglalás.")
+
+with open("helyfoglalas.txt", "rt", encoding="utf-8") as f:
+        fejlec=f.readline()
+        tartalom = f.read().split()
+ 
+        nezoter = [x for x in tartalom]
+ 
+        for y in range(0, len(nezoter), 10):
+            print(" ".join(nezoter[y:y+10]))
